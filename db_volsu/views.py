9af28452d946951e3ad09d7ec48cd2458deec8ec
Mernus <mernusv@gmail.com>
2020-05-28 19:44:49 +0400

    Work with cache

--- db_volsu/views.py
+++ db_volsu/views.py
@@ -1,35 +1,31 @@
-import os
 from configparser import ConfigParser
 
 from django.core.cache import cache
 from django.shortcuts import render
 
-from db_volsu.configs import params
 from db_volsu import settings
+from db_volsu.configs import params
 
 
 def base_page(request):
-    if request.method == "GET":
-        if cache.get("host", None) is None:
-            return render(request, 'login_page.html')
+    if request.method == "POST":
+        key_set = settings.CONNECTION_PARAMS
+        cache_data = {key: request.POST[key] for key in key_set}
+
+        # TODO: проверить, что есть все данные, в противном случае слать в жопу
+        cache_timeout = settings.CACHE_TTL
+        cache.set_many(cache_data, timeout=cache_timeout)
 
         return render(request, 'database.html')
 
-    key_set = settings.CONNECTION_PARAMS
-    cache_data = {key: request.POST[key] for key in key_set}
-
-    if not cache_data:
+    if cache.get("host", None) is None:
         parser = ConfigParser()
         parser.read(params.DEFAULTS_INI_FILE_PATH)
 
         if parser.has_section(params.DEFAULTS_SECTION_NAME):
             db_params = parser.items(section=params.DEFAULTS_SECTION_NAME)
             dict_params = {parameter[0]: parameter[1] for parameter in db_params}
-
-            cache_data.update(dict_params)
-
-    # TODO: проверить, что есть все данные, в противном случае слать в жопу
-    cache_timeout = settings.CACHE_TTL
-    cache.set_many(cache_data, timeout=cache_timeout)
+        # TODO: проверить, что dict_params объявлен
+        return render(request, 'login_page.html', context=dict_params)
 
     return render(request, 'database.html')
