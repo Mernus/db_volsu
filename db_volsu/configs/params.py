ed47a9df29c1f122263789d2726b270a1385ac28
Mernus <mernusv@gmail.com>
2020-06-11 18:13:11 +0400

    raws update

--- db_volsu/configs/params.py
+++ db_volsu/configs/params.py
@@ -13,47 +13,47 @@ STATION_RAW = "SELECT * FROM bus_schema.station"
 BUS_RAW = "SELECT bus_schema.bus.id, bus_type.firm, bus_type.seria FROM bus_schema.bus " \
           "LEFT JOIN bus_schema.bus_depot AS bus_type ON bus_type.id = bus_schema.bus.bus_type_id"
 
-SCHEDULE_RAW = "SELECT bus_schema.schedule.id," \
-               "bus_schema.schedule.departure, dep_station.station_name," \
-               "bus_schema.schedule.arrival, arr_station.station_name," \
-               "bus.id, bus.firm, bus.seria" \
-               "FROM bus_schema.schedule" \
-               "LEFT JOIN bus_schema.station AS dep_station ON dep_station.id = bus_schema.schedule.point_of_departure_id" \
-               "LEFT JOIN bus_schema.station AS arr_station ON arr_station.id = bus_schema.schedule.point_of_arrival_id" \
-               "LEFT JOIN" \
-               "(" \
-               "SELECT bus_schema.bus.id, bus_type.firm, bus_type.seria FROM bus_schema.bus" \
-               "LEFT JOIN bus_schema.bus_depot AS bus_type ON bus_type.id = bus_schema.bus.bus_type_id" \
-               ")" \
+SCHEDULE_RAW = "SELECT bus_schema.schedule.id, " \
+               "bus_schema.schedule.departure, dep_station.station_name, " \
+               "bus_schema.schedule.arrival, arr_station.station_name, " \
+               "bus.id, bus.firm, bus.seria " \
+               "FROM bus_schema.schedule " \
+               "LEFT JOIN bus_schema.station AS dep_station ON dep_station.id = bus_schema.schedule.point_of_departure_id " \
+               "LEFT JOIN bus_schema.station AS arr_station ON arr_station.id = bus_schema.schedule.point_of_arrival_id " \
+               "LEFT JOIN " \
+               "( " \
+               "SELECT bus_schema.bus.id, bus_type.firm, bus_type.seria FROM bus_schema.bus " \
+               "LEFT JOIN bus_schema.bus_depot AS bus_type ON bus_type.id = bus_schema.bus.bus_type_id " \
+               ") " \
                "AS bus ON bus.id = bus_schema.schedule.bus_id"
 
-TICKET_RAW = "SELECT bus_schema.ticket.id," \
-             "user_d.name, user_d.surname, user_d.mail," \
-             "bus_schema.ticket.passenger_name, bus_schema.ticket.passenger_surname," \
-             "arr_station.st_time, arr_station.st_name," \
-             "dep_station.st_time, dep_station.st_name," \
-             "bus_schema.ticket.cost" \
-             "FROM bus_schema.ticket" \
-             "LEFT JOIN bus_schema.user_data AS user_d ON user_d.id = bus_schema.ticket.buyer_id" \
-             "LEFT JOIN" \
-             "(" \
+TICKET_RAW = "SELECT bus_schema.ticket.id, " \
+             "user_d.name, user_d.surname, user_d.mail, " \
+             "bus_schema.ticket.passenger_name, bus_schema.ticket.passenger_surname, " \
+             "arr_station.st_time, arr_station.st_name, " \
+             "dep_station.st_time, dep_station.st_name, " \
+             "bus_schema.ticket.cost " \
+             "FROM bus_schema.ticket " \
+             "LEFT JOIN bus_schema.user_data AS user_d ON user_d.id = bus_schema.ticket.buyer_id " \
+             "LEFT JOIN " \
+             "( " \
              "SELECT " \
              "bus_schema.schedule.id AS st_id, " \
              "bus_schema.schedule.arrival AS st_time, " \
-             "arr_station.station_name AS st_name" \
-             "FROM bus_schema.schedule" \
+             "arr_station.station_name AS st_name " \
+             "FROM bus_schema.schedule " \
              "LEFT JOIN bus_schema.station AS arr_station " \
-             "ON arr_station.id = bus_schema.schedule.point_of_arrival_id" \
-             ")" \
-             "AS arr_station ON arr_station.st_id = bus_schema.ticket.route_id" \
-             "LEFT JOIN" \
-             "(" \
+             "ON arr_station.id = bus_schema.schedule.point_of_arrival_id " \
+             ") " \
+             "AS arr_station ON arr_station.st_id = bus_schema.ticket.route_id " \
+             "LEFT JOIN " \
+             "( " \
              "SELECT " \
              "bus_schema.schedule.id AS st_id, " \
              "bus_schema.schedule.departure AS st_time, " \
-             "dep_station.station_name AS st_name" \
-             "FROM bus_schema.schedule" \
+             "dep_station.station_name AS st_name " \
+             "FROM bus_schema.schedule " \
              "LEFT JOIN bus_schema.station AS dep_station " \
-             "ON dep_station.id = bus_schema.schedule.point_of_departure_id" \
-             ")" \
+             "ON dep_station.id = bus_schema.schedule.point_of_departure_id " \
+             ") " \
              "AS dep_station ON dep_station.st_id = bus_schema.ticket.route_id"
